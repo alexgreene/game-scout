7ba40656377c7d79a9f7afd90ceb0ad502fda8d4
Alex Greene <alexgrn7@gmail.com>
2017-02-13 17:45:22 -0800

    Tensorflow script updated to use SQL

--- wide-deep-train.py
+++ wide-deep-train.py
@@ -20,114 +20,192 @@ from __future__ import print_function
 import argparse
 import sys
 import tempfile
+import MySQLdb
 
 from six.moves import urllib
 
 import pandas as pd
 import tensorflow as tf
 
+from gamescout_db import db, cur
 
-COLUMNS = ["age", "workclass", "fnlwgt", "education", "education_num",
-           "marital_status", "occupation", "relationship", "race", "gender",
-           "capital_gain", "capital_loss", "hours_per_week", "native_country",
-           "income_bracket"]
-LABEL_COLUMN = "label"
-CATEGORICAL_COLUMNS = ["workclass", "education", "marital_status", "occupation",
-                       "relationship", "race", "gender", "native_country"]
-CONTINUOUS_COLUMNS = ["age", "education_num", "capital_gain", "capital_loss",
-                      "hours_per_week"]
+COLUMNS = [
+  'ONE_RUN_GAME',
+  'HT_WPCT',
+  'HT_WPCT_1R',
+  'HT_WPCT_2R',
+  'AT_WPCT',
+  'AT_WPCT_1R',
+  'AT_WPCT_2R',
+  'HT_RUN_DIFF',
+  'HT_AVG_RS_WIN',
+  'HT_AVG_RA_WIN',
+  'HT_AVG_RS_LOSS',
+  'HT_AVG_RA_LOSS',
+  'AT_RUN_DIFF',
+  'AT_AVG_RS_WIN',
+  'AT_AVG_RA_WIN',
+  'AT_AVG_RS_LOSS',
+  'AT_AVG_RA_LOSS',
+  'HP_RUNS_PER_9',
+  'HP_BB_PER_9',
+  'HP_H_PER_9',
+  'HP_K_PER_9',
+  'HP_IP',
+  'HP_ERA',
+  'HP_AVG_IP',
+  'AP_RUNS_PER_9',
+  'AP_BB_PER_9',
+  'AP_H_PER_9',
+  'AP_K_PER_9',
+  'AP_IP',
+  'AP_ERA',
+  'AP_AVG_IP',
+  'HT_P_AVG',
+  'HT_C_AVG',
+  'HT_1B_AVG',
+  'HT_2B_AVG',
+  'HT_3B_AVG',
+  'HT_SS_AVG',
+  'HT_LF_AVG',
+  'HT_CF_AVG',
+  'HT_RF_AVG',
+  'AT_P_AVG',
+  'AT_C_AVG',
+  'AT_1B_AVG',
+  'AT_2B_AVG',
+  'AT_3B_AVG',
+  'AT_SS_AVG',
+  'AT_LF_AVG',
+  'AT_CF_AVG',
+  'AT_RF_AVG',
+  'HT_AVG_HRS',
+  'AT_AVG_HRS']
 
-
-def maybe_download(train_data, test_data):
-  """Maybe downloads training data and returns train and test file names."""
-  if train_data:
-    train_file_name = train_data
-  else:
-    train_file = tempfile.NamedTemporaryFile(delete=False)
-    urllib.request.urlretrieve("http://mlr.cs.umass.edu/ml/machine-learning-databases/adult/adult.data", train_file.name)  # pylint: disable=line-too-long
-    train_file_name = train_file.name
-    train_file.close()
-    print("Training data is downloaded to %s" % train_file_name)
-
-  if test_data:
-    test_file_name = test_data
-  else:
-    test_file = tempfile.NamedTemporaryFile(delete=False)
-    urllib.request.urlretrieve("http://mlr.cs.umass.edu/ml/machine-learning-databases/adult/adult.test", test_file.name)  # pylint: disable=line-too-long
-    test_file_name = test_file.name
-    test_file.close()
-    print("Test data is downloaded to %s" % test_file_name)
-
-  return train_file_name, test_file_name
-
+LABEL_COLUMN = 'ONE_RUN_GAME'
 
 def build_estimator(model_dir, model_type):
   """Build an estimator."""
-  # Sparse base columns.
-  gender = tf.contrib.layers.sparse_column_with_keys(column_name="gender",
-                                                     keys=["female", "male"])
-  education = tf.contrib.layers.sparse_column_with_hash_bucket(
-      "education", hash_bucket_size=1000)
-  relationship = tf.contrib.layers.sparse_column_with_hash_bucket(
-      "relationship", hash_bucket_size=100)
-  workclass = tf.contrib.layers.sparse_column_with_hash_bucket(
-      "workclass", hash_bucket_size=100)
-  occupation = tf.contrib.layers.sparse_column_with_hash_bucket(
-      "occupation", hash_bucket_size=1000)
-  native_country = tf.contrib.layers.sparse_column_with_hash_bucket(
-      "native_country", hash_bucket_size=1000)
 
   # Continuous base columns.
-  age = tf.contrib.layers.real_valued_column("age")
-  education_num = tf.contrib.layers.real_valued_column("education_num")
-  capital_gain = tf.contrib.layers.real_valued_column("capital_gain")
-  capital_loss = tf.contrib.layers.real_valued_column("capital_loss")
-  hours_per_week = tf.contrib.layers.real_valued_column("hours_per_week")
-
-  # Transformations.
-  age_buckets = tf.contrib.layers.bucketized_column(age,
-                                                    boundaries=[
-                                                        18, 25, 30, 35, 40, 45,
-                                                        50, 55, 60, 65
-                                                    ])
+  HT_WPCT = tf.contrib.layers.real_valued_column("HT_WPCT")
+  HT_WPCT_1R = tf.contrib.layers.real_valued_column("HT_WPCT_1R")
+  HT_WPCT_2R = tf.contrib.layers.real_valued_column("HT_WPCT_2R")
+  AT_WPCT = tf.contrib.layers.real_valued_column("AT_WPCT")
+  AT_WPCT_1R = tf.contrib.layers.real_valued_column("AT_WPCT_1R")
+  AT_WPCT_2R = tf.contrib.layers.real_valued_column("AT_WPCT_2R")
+  HT_RUN_DIFF = tf.contrib.layers.real_valued_column("HT_RUN_DIFF")
+  HT_AVG_RS_WIN = tf.contrib.layers.real_valued_column("HT_AVG_RS_WIN")
+  HT_AVG_RA_WIN = tf.contrib.layers.real_valued_column("HT_AVG_RA_WIN")
+  HT_AVG_RS_LOSS = tf.contrib.layers.real_valued_column("HT_AVG_RS_LOSS")
+  HT_AVG_RA_LOSS = tf.contrib.layers.real_valued_column("HT_AVG_RA_LOSS")
+  AT_RUN_DIFF = tf.contrib.layers.real_valued_column("AT_RUN_DIFF")
+  AT_AVG_RS_WIN = tf.contrib.layers.real_valued_column("AT_AVG_RS_WIN")
+  AT_AVG_RA_WIN = tf.contrib.layers.real_valued_column("AT_AVG_RA_WIN")
+  AT_AVG_RS_LOSS = tf.contrib.layers.real_valued_column("AT_AVG_RS_LOSS")
+  AT_AVG_RA_LOSS = tf.contrib.layers.real_valued_column("AT_AVG_RA_LOSS")
+  HP_RUNS_PER_9 = tf.contrib.layers.real_valued_column("HP_RUNS_PER_9")
+  HP_BB_PER_9 = tf.contrib.layers.real_valued_column("HP_BB_PER_9")
+  HP_H_PER_9 = tf.contrib.layers.real_valued_column("HP_H_PER_9")
+  HP_K_PER_9 = tf.contrib.layers.real_valued_column("HP_K_PER_9")
+  HP_IP = tf.contrib.layers.real_valued_column("HP_IP")
+  HP_ERA = tf.contrib.layers.real_valued_column("HP_ERA")
+  HP_AVG_IP = tf.contrib.layers.real_valued_column("HP_AVG_IP")
+  AP_RUNS_PER_9 = tf.contrib.layers.real_valued_column("AP_RUNS_PER_9")
+  AP_BB_PER_9 = tf.contrib.layers.real_valued_column("AP_BB_PER_9")
+  AP_H_PER_9 = tf.contrib.layers.real_valued_column("AP_H_PER_9")
+  AP_K_PER_9 = tf.contrib.layers.real_valued_column("AP_K_PER_9")
+  AP_IP = tf.contrib.layers.real_valued_column("AP_IP")
+  AP_ERA = tf.contrib.layers.real_valued_column("AP_ERA")
+  AP_AVG_IP = tf.contrib.layers.real_valued_column("AP_AVG_IP")
+  HT_P_AVG = tf.contrib.layers.real_valued_column("HT_P_AVG")
+  HT_C_AVG = tf.contrib.layers.real_valued_column("HT_C_AVG")
+  HT_1B_AVG = tf.contrib.layers.real_valued_column("HT_1B_AVG")
+  HT_2B_AVG = tf.contrib.layers.real_valued_column("HT_2B_AVG")
+  HT_3B_AVG = tf.contrib.layers.real_valued_column("HT_3B_AVG")
+  HT_SS_AVG = tf.contrib.layers.real_valued_column("HT_SS_AVG")
+  HT_LF_AVG = tf.contrib.layers.real_valued_column("HT_LF_AVG")
+  HT_CF_AVG = tf.contrib.layers.real_valued_column("HT_CF_AVG")
+  HT_RF_AVG = tf.contrib.layers.real_valued_column("HT_RF_AVG")
+  AT_P_AVG = tf.contrib.layers.real_valued_column("AT_P_AVG")
+  AT_C_AVG = tf.contrib.layers.real_valued_column("AT_C_AVG")
+  AT_1B_AVG = tf.contrib.layers.real_valued_column("AT_1B_AVG")
+  AT_2B_AVG = tf.contrib.layers.real_valued_column("AT_2B_AVG")
+  AT_3B_AVG = tf.contrib.layers.real_valued_column("AT_3B_AVG")
+  AT_SS_AVG = tf.contrib.layers.real_valued_column("AT_SS_AVG")
+  AT_LF_AVG = tf.contrib.layers.real_valued_column("AT_LF_AVG")
+  AT_CF_AVG = tf.contrib.layers.real_valued_column("AT_CF_AVG")
+  AT_RF_AVG = tf.contrib.layers.real_valued_column("AT_RF_AVG")
+  HT_AVG_HRS = tf.contrib.layers.real_valued_column("HT_AVG_HRS")
+  AT_AVG_HRS = tf.contrib.layers.real_valued_column("AT_AVG_HRS")
 
   # Wide columns and deep columns.
-  wide_columns = [gender, native_country, education, occupation, workclass,
-                  relationship, age_buckets,
-                  tf.contrib.layers.crossed_column([education, occupation],
-                                                   hash_bucket_size=int(1e4)),
-                  tf.contrib.layers.crossed_column(
-                      [age_buckets, education, occupation],
-                      hash_bucket_size=int(1e6)),
-                  tf.contrib.layers.crossed_column([native_country, occupation],
-                                                   hash_bucket_size=int(1e4))]
-  deep_columns = [
-      tf.contrib.layers.embedding_column(workclass, dimension=8),
-      tf.contrib.layers.embedding_column(education, dimension=8),
-      tf.contrib.layers.embedding_column(gender, dimension=8),
-      tf.contrib.layers.embedding_column(relationship, dimension=8),
-      tf.contrib.layers.embedding_column(native_country,
-                                         dimension=8),
-      tf.contrib.layers.embedding_column(occupation, dimension=8),
-      age,
-      education_num,
-      capital_gain,
-      capital_loss,
-      hours_per_week,
+  _columns = [
+    HT_WPCT,
+    HT_WPCT_1R,
+    HT_WPCT_2R,
+    AT_WPCT,
+    AT_WPCT_1R,
+    AT_WPCT_2R,
+    HT_RUN_DIFF,
+    HT_AVG_RS_WIN,
+    HT_AVG_RA_WIN,
+    HT_AVG_RS_LOSS,
+    HT_AVG_RA_LOSS,
+    AT_RUN_DIFF,
+    AT_AVG_RS_WIN,
+    AT_AVG_RA_WIN,
+    AT_AVG_RS_LOSS,
+    AT_AVG_RA_LOSS,
+    HP_RUNS_PER_9,
+    HP_BB_PER_9,
+    HP_H_PER_9,
+    HP_K_PER_9,
+    HP_IP,
+    HP_ERA,
+    HP_AVG_IP,
+    AP_RUNS_PER_9,
+    AP_BB_PER_9,
+    AP_H_PER_9,
+    AP_K_PER_9,
+    AP_IP,
+    AP_ERA,
+    AP_AVG_IP,
+    HT_P_AVG,
+    HT_C_AVG,
+    HT_1B_AVG,
+    HT_2B_AVG,
+    HT_3B_AVG,
+    HT_SS_AVG,
+    HT_LF_AVG,
+    HT_CF_AVG,
+    HT_RF_AVG,
+    AT_P_AVG,
+    AT_C_AVG,
+    AT_1B_AVG,
+    AT_2B_AVG,
+    AT_3B_AVG,
+    AT_SS_AVG,
+    AT_LF_AVG,
+    AT_CF_AVG,
+    AT_RF_AVG,
+    HT_AVG_HRS,
+    AT_AVG_HRS
   ]
+
 
   if model_type == "wide":
     m = tf.contrib.learn.LinearClassifier(model_dir=model_dir,
-                                          feature_columns=wide_columns)
+                                          feature_columns=_columns)
   elif model_type == "deep":
     m = tf.contrib.learn.DNNClassifier(model_dir=model_dir,
-                                       feature_columns=deep_columns,
+                                       feature_columns=_columns,
                                        hidden_units=[100, 50])
   else:
     m = tf.contrib.learn.DNNLinearCombinedClassifier(
         model_dir=model_dir,
-        linear_feature_columns=wide_columns,
-        dnn_feature_columns=deep_columns,
+        linear_feature_columns=_columns,
+        dnn_feature_columns=_columns,
         dnn_hidden_units=[100, 50])
   return m
 
@@ -136,18 +214,10 @@ def input_fn(df):
   """Input builder function."""
   # Creates a dictionary mapping from each continuous feature column name (k) to
   # the values of that column stored in a constant Tensor.
-  continuous_cols = {k: tf.constant(df[k].values) for k in CONTINUOUS_COLUMNS}
-  # Creates a dictionary mapping from each categorical feature column name (k)
-  # to the values of that column stored in a tf.SparseTensor.
-  categorical_cols = {
-      k: tf.SparseTensor(
-          indices=[[i, 0] for i in range(df[k].size)],
-          values=df[k].values,
-          shape=[df[k].size, 1])
-      for k in CATEGORICAL_COLUMNS}
+  continuous_cols = {k: tf.constant(df[k].values) for k in COLUMNS}
+
   # Merges the two dictionaries into one.
   feature_cols = dict(continuous_cols)
-  feature_cols.update(categorical_cols)
   # Converts the label column into a constant Tensor.
   label = tf.constant(df[LABEL_COLUMN].values)
   # Returns the feature columns and the label.
@@ -156,27 +226,13 @@ def input_fn(df):
 
 def train_and_eval(model_dir, model_type, train_steps, train_data, test_data):
   """Train and evaluate the model."""
-  train_file_name, test_file_name = maybe_download(train_data, test_data)
-  df_train = pd.read_csv(
-      tf.gfile.Open(train_file_name),
-      names=COLUMNS,
-      skipinitialspace=True,
-      engine="python")
-  df_test = pd.read_csv(
-      tf.gfile.Open(test_file_name),
-      names=COLUMNS,
-      skipinitialspace=True,
-      skiprows=1,
-      engine="python")
+
+  df_train = pd.read_sql('select * from GamePrediction;', columns=COLUMNS, con=db) 
+  df_test = pd.read_sql('select * from GamePrediction;', columns=COLUMNS, con=db) 
 
   # remove NaN elements
   df_train = df_train.dropna(how='any', axis=0)
   df_test = df_test.dropna(how='any', axis=0)
-
-  df_train[LABEL_COLUMN] = (
-      df_train["income_bracket"].apply(lambda x: ">50K" in x)).astype(int)
-  df_test[LABEL_COLUMN] = (
-      df_test["income_bracket"].apply(lambda x: ">50K" in x)).astype(int)
 
   model_dir = tempfile.mkdtemp() if not model_dir else model_dir
   print("model directory = %s" % model_dir)
@@ -187,14 +243,11 @@ def train_and_eval(model_dir, model_type, train_steps, train_data, test_data):
   for key in sorted(results):
     print("%s: %s" % (key, results[key]))
 
-
 FLAGS = None
-
 
 def main(_):
   train_and_eval(FLAGS.model_dir, FLAGS.model_type, FLAGS.train_steps,
                  FLAGS.train_data, FLAGS.test_data)
-
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
@@ -231,3 +284,5 @@ if __name__ == "__main__":
   )
   FLAGS, unparsed = parser.parse_known_args()
   tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+
+  mysql_cn.close()
